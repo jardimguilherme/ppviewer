5d560a521dd73fa939edd69cfb929019d0d221ac
Guilherme Jardim <guilhermejardim@guilhermes-mbp.lan>
2021-08-06 21:32:05 -0400

    all features implemented

--- public/JS/createJson.js
+++ public/JS/createJson.js
@@ -1,4 +1,4 @@
-const createJson = (policy) => {
+const createJson = () => {
     var isCaptcha = -1;
     var isCookies = -1;
     var isLinksAtPageBottom = -1;
@@ -14,6 +14,19 @@ const createJson = (policy) => {
     document.getElementById('languageList').checked ? isLanguageList = 1 : isLanguageList = 0;
     document.getElementById('appList').checked ? isAppList = 1 : isAppList = 0;
     document.getElementById('qa').checked ? isQa = 1 : isQa = 0;
+
+    var isPolicy = document.getElementById("isPolicy-input").checked;
+    var isntPolicy = document.getElementById("isntPolicy-input").checked;
+
+    if (isPolicy == true && isntPolicy == false) {
+        policy = 1;
+    }
+    else if (isPolicy == false && isntPolicy == true) {
+        policy = 0;
+    }
+    else if (isPolicy == false && isntPolicy == false) {
+        policy = -1;
+    }
 
     var title = document.getElementById('app-title').innerHTML;
     var lang = document.getElementById('app-lang').innerHTML;
--- public/JS/scripts.js
+++ public/JS/scripts.js
@@ -4,8 +4,9 @@ const next = () => {
   document.getElementById("id").innerHTML = toNum;
   document.getElementById("isPolicy").style.display = "none";
   document.getElementById("isntPolicy").style.display = "none";
-
   getData(toNum);
+  uncheckAll();
+  check();
 }
 
 const previous = () => {
@@ -16,6 +17,8 @@ const previous = () => {
   document.getElementById("isPolicy").style.display = "none";
   document.getElementById("isntPolicy").style.display = "none";
   getData(toNum);
+  uncheckAll();
+  check();
 }
 
 const uncheckAll = () => {
@@ -28,20 +31,20 @@ const check = () => {
   var isPolicy = document.getElementById("isPolicy-input").checked;
   var isntPolicy = document.getElementById("isntPolicy-input").checked;
 
-  if(isPolicy == true && isntPolicy == false) {
+  if (isPolicy == true && isntPolicy == false) {
     document.getElementById("isntPolicy").style.display = "none";
     document.getElementById("isPolicy").style.display = "block";
     document.getElementById("isntPolicy-input").disabled = true;
   }
-  else if(isPolicy == true && isntPolicy == true) {
+  else if (isPolicy == true && isntPolicy == true) {
     console.log("não pode");
   }
-  else if(isPolicy == false && isntPolicy == true) {
+  else if (isPolicy == false && isntPolicy == true) {
     document.getElementById("isPolicy").style.display = "none";
     document.getElementById("isntPolicy").style.display = "block";
     document.getElementById("isPolicy-input").disabled = true;
   }
-  else if(isPolicy == false && isntPolicy == false) {
+  else if (isPolicy == false && isntPolicy == false) {
     document.getElementById("isPolicy").style.display = "none";
     document.getElementById("isntPolicy").style.display = "none";
     document.getElementById("isPolicy-input").disabled = false;
@@ -53,4 +56,32 @@ const goTo = () => {
   let i = document.getElementById("id").innerHTML;
   var toNum = parseInt(i);
   getData(toNum);
+}
+
+const fillData = (array) => {
+  var index = document.getElementById('id').innerHTML;
+
+  if (array[index]['isPolicy'] == 1) {
+    document.getElementById('isPolicy-input').checked = true;
+    check();
+    if (array[index]['isLanguageList'] == 1)
+      document.getElementById('languageList').checked = true;
+    if (array[index]['isQa'] == 1)
+      document.getElementById('qa').checked = true;
+  }
+
+  if (array[index]['isPolicy'] == 0) {
+    document.getElementById('isntPolicy-input').checked = true;
+    check();
+    if (array[index]['isAppList'] == 1)
+      document.getElementById('appList').checked = true;
+    if (array[index]['isCaptcha'] == 1)
+      document.getElementById('captcha').checked = true;
+    if (array[index]['isCookies'] == 1)
+      document.getElementById('cookies').checked = true;
+    if (array[index]['isLinksAtPageBottom'] == 1)
+      document.getElementById('linksAtPageBottom').checked = true;
+    if (array[index]['isFeatureList'] == 1)
+      document.getElementById('featureList').checked = true;
+  }
 }
